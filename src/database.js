4736885be970f6e81210e38793de924094fde24e
Emmanuel Omar Alatorre Leon <developeromar@yahoo.com>
2021-11-20 05:29:35 -0600

    set new connection

--- src/database.js
+++ src/database.js
@@ -6,10 +6,13 @@ const connection = {};
 async function dbConnect() {
   if (connection.isConnected) return;
 
-  const db = await mongoose.connect("mongodb://localhost/iventas", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const db = await mongoose.connect(
+    "mongodb+srv://user_1:7l18gvvFk6aIgXgM@cluster0.l8zli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  );
 
   connection.isConnected = db.connections[0].readyState;
   console.log(connection.isConnected);
